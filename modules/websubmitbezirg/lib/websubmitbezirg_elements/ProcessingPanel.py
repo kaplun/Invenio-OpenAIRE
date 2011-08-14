0a8e23e0dd103541ff61cba0a15e53eaa8b63eea
Nikolaos Bezirgiannis <bezirg@gmail.com>
2011-08-14 16:10:48 +0300

    Processing takes too long feature

--- modules/websubmitbezirg/lib/websubmitbezirg_elements/ProcessingPanel.py
+++ modules/websubmitbezirg/lib/websubmitbezirg_elements/ProcessingPanel.py
@@ -2,6 +2,7 @@ import sys
 if sys.version_info[3] == 'pyjamas':
     from pyjamas.ui.VerticalPanel import VerticalPanel
     from pyjamas.ui.HTML import HTML
+    from pyjamas.Timer import Timer
 else:
     class NullClass(object):
         def __init__(self, *args, **kwargs):
@@ -17,8 +18,14 @@ class ProcessingPanel(VerticalPanel):
         #_ = gettext_set_language(lang)
         super(ProcessingPanel, self).__init__(**kwargs)
         self.loader = Loader()
-        self.label = HTML("Processing, please wait...")
+        self.label = HTML("Loading...")
         self.load()
+        self.t = Timer(10000, notify=self)
     def load(self):
         self.add(self.loader)
         self.add(self.label)
+    def onTimer(self, timer):
+        self.label.setHTML("""
+        It seems that Processing takes too long. <br /> 
+        You can try to hit <a href="">Refresh</a> or bookmark the page and come back later. <br />
+        An e-mail notification will be automatically sent to you when the Processing is complete.""")
